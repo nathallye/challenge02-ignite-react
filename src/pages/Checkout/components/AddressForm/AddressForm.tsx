a9e393bebc08f2e1d6214b4ea1363abb78b9224b
Nathallye Bacelar <nathallyet@gmail.com>
2023-03-15 22:14:07 -0300

    removing comments

--- src/pages/Checkout/components/AddressForm/AddressForm.tsx
+++ src/pages/Checkout/components/AddressForm/AddressForm.tsx
@@ -32,7 +32,6 @@ export const AddressForm = () => {
 
   return (
     <AddressFormContainer>
-      {/* TODO: Verificar lib que busca endereço pelo cep e fazer alto complete */}
       <Input
         placeholder="CEP"
         type="number"
